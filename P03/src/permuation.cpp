4def7c8d2cfe8688bcde9aaeaf73dadb687ea8b9
JoelHernandez343 <joel.programador@gmail.com>
2020-02-17 09:53:16 -0600

    Empezando practica 2

---
+++ P03/src/permuation.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <tuple>
+
+// #include "permutation.hpp"
+
+int main(void){
+
+    std::tuple t(1,'a',3);
+
+    return 0;
+}
---
+++ P03/src/permutation.hpp
@@ -0,0 +1,16 @@
+#ifndef J_PERMUTATION_HEADER_FUNCTIONS
+#define J_PERMUTATION_HEADER_FUNCTIONS
+
+#include <iostream>
+#include <vector>
+#include <utility>
+
+std::vector< std::pair<int, int> > makePermutation(int n){
+    std::vector< std::pair<int, int> > r (n);
+    for (int i = 0; i < n; ++i){
+        auto t = std::tuple(1, 1.0, 'a');
+    }
+
+}
+
+#endif
