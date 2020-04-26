e97bfd2f896e634e4cab4e0b6d1723a67bc7349c
Joel Hernandez <joel.programador@gmail.com>
2020-04-25 23:19:44 -0500

    Block ciphers done

--- P04/code_examples/block_ciphers/src/idea.cpp
+++ P04/code_examples/block_ciphers/src/idea.cpp
@@ -1,5 +1,5 @@
 /*
- * Example of AES usage with CBC mode of operation
+ * Example of IDEA (International Data Encryption Standard by Massey) usage with CBC mode of operation
  */
 
 #include <iostream>
@@ -9,7 +9,7 @@
 #include <cryptopp/modes.h>    // For modes
 #include <cryptopp/osrng.h>    // For AutoSeededRandomPool
 #include <cryptopp/hex.h>      // For HexEncoder
-#include <cryptopp/aes.h>
+#include <cryptopp/idea.h>
 
 // Convert a byte block to its hexadecimal representation
 std::string byteBlockToString(CryptoPP::SecByteBlock block) {
@@ -24,8 +24,8 @@ std::string byteBlockToString(CryptoPP::SecByteBlock block) {
 
 }
 
-// Encrypting AES / CBC mode
-std::string encrypt(std::string input, CryptoPP::CBC_Mode<CryptoPP::AES>::Encryption encryptor) {
+// Encrypting IDEA / CBC mode
+std::string encrypt(std::string input, CryptoPP::CBC_Mode<CryptoPP::IDEA>::Encryption encryptor) {
     std::string result;
 
     CryptoPP::StringSource ss (input, true, new CryptoPP::StreamTransformationFilter(encryptor, new CryptoPP::StringSink( result )));
@@ -33,8 +33,8 @@ std::string encrypt(std::string input, CryptoPP::CBC_Mode<CryptoPP::AES>::Encryp
     return result;
 }
 
-// Decrypting AES / CBC mode
-std::string decrypt(std::string input, CryptoPP::CBC_Mode<CryptoPP::AES>::Decryption decryptor) {
+// Decrypting IDEA / CBC mode
+std::string decrypt(std::string input, CryptoPP::CBC_Mode<CryptoPP::IDEA>::Decryption decryptor) {
     std::string result;
 
     CryptoPP::StringSource ss (input, true, new CryptoPP::StreamTransformationFilter(decryptor, new CryptoPP::StringSink( result )));
@@ -58,22 +58,22 @@ int main(void) {
     CryptoPP::AutoSeededRandomPool rand;
 
     // Generating a random key
-    CryptoPP::SecByteBlock key(0x00, CryptoPP::AES::DEFAULT_KEYLENGTH);
+    CryptoPP::SecByteBlock key(0x00, CryptoPP::IDEA::DEFAULT_KEYLENGTH);
     rand.GenerateBlock(key, key.size());
     std::cout << "Using key: " << byteBlockToString(key) << "\n";
 
     // Generating a random IV
-    CryptoPP::SecByteBlock iv(0x00, CryptoPP::AES::BLOCKSIZE);
+    CryptoPP::SecByteBlock iv(0x00, CryptoPP::IDEA::BLOCKSIZE);
     rand.GenerateBlock(iv, iv.size());
     std::cout << "Using IV: " << byteBlockToString(iv) << "\n";
 
     // Input message
-    std::string input = "Hello world!, this is a very long message to prove the AES encryption with CBC mode provided by the Crypto++ library!";
+    std::string input = "Hello world!, this is a very long message to prove the IDEA encryption with CBC mode provided by the Crypto++ library!";
     std::string cipher, encoded, recovered;
 
     // CBC Encryptor and Decryptor
-    CryptoPP::CBC_Mode<CryptoPP::AES>::Encryption encryptor(key, key.size(), iv);
-    CryptoPP::CBC_Mode<CryptoPP::AES>::Decryption decryptor(key, key.size(), iv);
+    CryptoPP::CBC_Mode<CryptoPP::IDEA>::Encryption encryptor(key, key.size(), iv);
+    CryptoPP::CBC_Mode<CryptoPP::IDEA>::Decryption decryptor(key, key.size(), iv);
 
     // Encrypting
     cipher = encrypt(input, encryptor);
